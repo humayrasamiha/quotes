5a8b6d80ffe6894490a7455eca05417c1e97823d
Humayra Samiha <samihajui25@gmail.com>
2019-07-27 14:04:12 +0600

    first commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quotes"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.panel-body {
+  font-family: "Arizonia", cursive;
+  font-size: 24px;
+  color: #6e6e6e;
+}
+.quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: #da9d9d;
+}
+</style>
